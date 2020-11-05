a7eb7d48db5a16c3c5f31d8a7a59a1f82b11641f
aheil2 <71800212+aheil2@users.noreply.github.com>
2020-11-04 20:46:36 -0600

    added a grand total row and calculation

--- code.js
+++ code.js
@@ -2,21 +2,28 @@ $(document).ready(function () {
     $("#textBoxOne").change(textboxOneChange);
     $("#textBoxTwo").change(textboxTwoChange);
     //run the change on page load to display the total for default values
+    var grandTotal = 0;
+    var totalPriceOne = 0;
+    var totalPriceTwo = 0;
     textboxOneChange();
     textboxTwoChange();
     function textboxOneChange() {
         var character = $("#textBoxOne").val();
         character = parseFloat(character);
-        var totalPrice = character * 39.99;
+        totalPriceOne = character * 39.99;
+        grandTotal = totalPriceOne + totalPriceTwo;
         // display
-        $("#bookOnePrice").text(totalPrice);
+        $("#bookOnePrice").text("$"+totalPriceOne.toFixed(2));
+        $("#grandTotal").text("$" + grandTotal.toFixed(2));
     }
-    function textboxTwoChange(event) {
+    function textboxTwoChange() {
         var character = $("#textBoxTwo").val();
         character = parseFloat(character);
-        var totalPrice = character * 14.99;
+        totalPriceTwo = character * 14.99;
+        grandTotal = totalPriceOne + totalPriceTwo;
         // display
-        $("#bookTwoPrice").text(totalPrice);
+        $("#bookTwoPrice").text("$"+totalPriceTwo.toFixed(2));
+        $("#grandTotal").text("$" + grandTotal.toFixed(2));
     }
 });
 
